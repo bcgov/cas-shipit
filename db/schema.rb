4621eb8b17c93417908a75fd2e5358276c54d3b3
Matthieu Foucault <matthieu@button.is>
2020-12-11 16:49:07 -0800

    feat: recreate shipit app with rails 6

--- db/schema.rb
+++ db/schema.rb
@@ -2,15 +2,15 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_03_173525) do
+ActiveRecord::Schema.define(version: 2020_12_11_214508) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 2019_09_03_173525) do
     t.string "api_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "sha", limit: 40
     t.index ["commit_id", "task_id"], name: "index_commit_deployments_on_commit_id_and_task_id", unique: true
     t.index ["task_id"], name: "index_commit_deployments_on_task_id"
   end
@@ -80,6 +81,7 @@ ActiveRecord::Schema.define(version: 2019_09_03_173525) do
     t.integer "pull_request_id"
     t.boolean "locked", default: false, null: false
     t.integer "lock_author_id"
+    t.string "pull_request_head_sha", limit: 40
     t.index ["author_id"], name: "index_commits_on_author_id"
     t.index ["committer_id"], name: "index_commits_on_committer_id"
     t.index ["created_at"], name: "index_commits_on_created_at"
@@ -193,9 +195,15 @@ ActiveRecord::Schema.define(version: 2019_09_03_173525) do
     t.index ["user_id"], name: "index_release_statuses_on_user_id"
   end
 
+  create_table "repositories", force: :cascade do |t|
+    t.string "owner", limit: 39, null: false
+    t.string "name", limit: 100, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["owner", "name"], name: "repository_unicity", unique: true
+  end
+
   create_table "stacks", id: :serial, force: :cascade do |t|
-    t.string "repo_name", limit: 100, null: false
-    t.string "repo_owner", limit: 39, null: false
     t.string "environment", limit: 50, default: "production", null: false
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -214,7 +222,11 @@ ActiveRecord::Schema.define(version: 2019_09_03_173525) do
     t.datetime "locked_since"
     t.boolean "merge_queue_enabled", default: false, null: false
     t.datetime "last_deployed_at"
-    t.index ["repo_owner", "repo_name", "environment"], name: "stack_unicity", unique: true
+    t.bigint "repository_id", null: false
+    t.datetime "archived_since"
+    t.index ["archived_since"], name: "index_stacks_on_archived_since"
+    t.index ["repository_id", "environment"], name: "stack_unicity", unique: true
+    t.index ["repository_id"], name: "index_stacks_on_repository_id"
   end
 
   create_table "statuses", id: :serial, force: :cascade do |t|
@@ -231,8 +243,8 @@ ActiveRecord::Schema.define(version: 2019_09_03_173525) do
 
   create_table "tasks", id: :serial, force: :cascade do |t|
     t.integer "stack_id", null: false
-    t.integer "since_commit_id", null: false
-    t.integer "until_commit_id", null: false
+    t.integer "since_commit_id"
+    t.integer "until_commit_id"
     t.string "status", limit: 10, default: "pending", null: false
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -252,11 +264,13 @@ ActiveRecord::Schema.define(version: 2019_09_03_173525) do
     t.datetime "ended_at"
     t.boolean "ignored_safeties", default: false, null: false
     t.integer "aborted_by_id"
+    t.integer "rollback_once_aborted_to_id"
     t.index ["rolled_up", "created_at", "status"], name: "index_tasks_on_rolled_up_and_created_at_and_status"
     t.index ["since_commit_id"], name: "index_tasks_on_since_commit_id"
     t.index ["stack_id", "allow_concurrency", "status"], name: "index_active_tasks"
     t.index ["stack_id", "allow_concurrency"], name: "index_tasks_on_stack_id_and_allow_concurrency"
     t.index ["stack_id", "status", "type"], name: "index_tasks_by_stack_and_status"
+    t.index ["status"], name: "index_tasks_on_status"
     t.index ["type", "stack_id", "parent_id"], name: "index_tasks_by_stack_and_parent"
     t.index ["until_commit_id"], name: "index_tasks_on_until_commit_id"
     t.index ["user_id"], name: "index_tasks_on_user_id"
